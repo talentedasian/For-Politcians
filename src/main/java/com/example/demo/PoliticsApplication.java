3421df6584e8e7afaf114ac130102b8e7746ad62
talentedasian <lucenaalmirjames123@gmail.com>
2021-04-29 21:56:15 +0800

    add facebook oauth2 properties

--- src/main/java/com/example/demo/PoliticsApplication.java
+++ src/main/java/com/example/demo/PoliticsApplication.java
@@ -1,18 +1,13 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PoliticsApplication {
 	
-	@Value("${spring.security.oauth2.client.registration.facebook.cliend-id}")
-	static String id;
-
 	public static void main(String[] args) {
 		SpringApplication.run(PoliticsApplication.class, args);
-		System.out.println(id);
 	}
 
 }
