3c117c16427882997c5f0449c38e08337e575f32
talentedasian <lucenaalmirjames123@gmail.com>
2021-04-29 21:46:52 +0800

    ignore application listener

--- src/main/java/com/example/demo/PoliticsApplication.java
+++ src/main/java/com/example/demo/PoliticsApplication.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
-import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class PoliticsApplication {
