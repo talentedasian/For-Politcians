5c29eecda6feb89078f0854f3d6a180c03abfd2e
talentedasian <lucenaalmirjames123@gmail.com>
2021-04-30 20:50:54 +0800

    config for oauth2 facebook

--- src/main/java/com/example/demo/PoliticsApplication.java
+++ src/main/java/com/example/demo/PoliticsApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { 
-        SecurityAutoConfiguration.class 
-    })
 public class PoliticsApplication {
 	
 	public static void main(String[] args) {
