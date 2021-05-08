c3b4772a16ca5d5508f219b328476dd053312cc1
talentedasian <lucenaalmirjames123@gmail.com>
2021-05-08 13:01:54 +0800

    add tests for decoded jwt

--- src/test/java/com/example/demo/unit/jwt/JwtProviderTest.java
+++ src/test/java/com/example/demo/unit/jwt/JwtProviderTest.java
@@ -12,13 +12,16 @@ import org.junit.jupiter.api.Test;
 import com.example.demo.jwt.JwtKeys;
 import com.example.demo.jwt.JwtProvider;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 
 public class JwtProviderTest {
 
 	@Test
-	public void assertEqualsId() {
+	public void assertEqualsEncodedJwt() {
 		Date dateNow = new Date(System.currentTimeMillis() + 3600000L);
+		
 		String actualJwts = Jwts.builder()
 				.signWith(JwtKeys.getJwtKeyPair().getPrivate())
 				.setSubject("test@gmail.com")
@@ -29,6 +32,26 @@ public class JwtProviderTest {
 		
 		String jwts =JwtProvider.createJwtWithDynamicExpirationDate("test@gmail.com", "test", dateNow);
 		
-		assertThat(actualJwts, equalTo(jwts));
+		assertThat(actualJwts, 
+				equalTo(jwts));
+	}
+	
+	@Test
+	public void assertEqualsDecodedJwt() {
+		Date dateNow = new Date(System.currentTimeMillis() + 3600000L);
+		String encodedJwts = JwtProvider.createJwtWithDynamicExpirationDate("test@gmail.com", "test", dateNow);
+		
+		Jws<Claims> decodedJwts = JwtProvider.decodeJwt(encodedJwts);
+		
+		Claims actualJwts = decodedJwts.getBody();
+		
+		assertThat("test@gmail.com", 
+				equalTo(actualJwts.getSubject()));
+		assertThat("test", 
+				equalTo(actualJwts.getId()));
+		assertThat(dateNow.toString(), 
+				equalTo(actualJwts.getExpiration().toString()));
+		assertThat(true, 
+				equalTo(actualJwts.getExpiration().before(dateNow)));
 	}
 }
