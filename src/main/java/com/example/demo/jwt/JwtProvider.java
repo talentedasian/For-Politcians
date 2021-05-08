4f7c3fadfc49839edcb55b66975fdd0f6d349d3d
talentedasian <lucenaalmirjames123@gmail.com>
2021-05-08 12:39:19 +0800

    added jwt encoder and decoder

--- src/main/java/com/example/demo/jwt/JwtProvider.java
+++ src/main/java/com/example/demo/jwt/JwtProvider.java
@@ -1,8 +1,36 @@
 package com.example.demo.jwt;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 
 public class JwtProvider {
 
-	public static Jwts
+	public static String createJwt(String sub, String id) {
+		
+		String jwts = Jwts.builder()
+				.signWith(JwtKeys.getJwtKeyPair().getPrivate())
+				.setSubject(sub)
+				.setId(id)
+				.setExpiration(new Date(System.currentTimeMillis() + 3600000L))
+				.setHeaderParam("login_mechanism", "facebook")
+				.compact();
+		
+		return jwts;
+	}
+	
+	public static Jws<Claims> decodeJwt(String jwt) {
+		Jws<Claims> jwts = Jwts.parserBuilder()
+				.setSigningKey(JwtKeys.getJwtKeyPair().getPublic())
+				.setAllowedClockSkewSeconds(60 * 3)
+				.build()
+				.parseClaimsJws(jwt);
+
+		return jwts;
+	}
 }
+
