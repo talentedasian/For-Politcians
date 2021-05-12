e8986d93cd73928281486d4152aaf0352cd0f9b1
talentedasian <lucenaalmirjames123@gmail.com>
2021-05-12 23:01:46 +0800

    remove clientRepo bean and replace with environment vars

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,9 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
@@ -27,8 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private RestTemplate template;
-	@Autowired
-	private ClientRegistrationRepository clientRepo;
+	
+	@Value("${spring.security.oauth2.client.registration.facebook.clientSecret}")
+	private String OAUTH2_SECRET;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -58,13 +62,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 		
 	public OAuth2AuthorizedClientRepository authorizedClientRepo() {
-		return new CustomOauth2AuthorizedClientsRepository(template, clientRepo.findByRegistrationId("facebook"));
+		return new CustomOauth2AuthorizedClientsRepository(template, this.facebookClientRegistration());
 	}
 	
 	@Bean
 	public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestsRepo() {
 		return new CustomOauth2AuthorizationRequestsRepository();
 	}
+	
+	private ClientRegistration facebookClientRegistration() {
+        return ClientRegistration.withRegistrationId("facebook")
+            .clientId("697702354184763")
+            .clientSecret(OAUTH2_SECRET)
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .redirectUri("{baseUrl}/login/oauth2/code/facebook")
+            .authorizationUri("https://www.facebook.com/dialog/oauth")
+            .tokenUri("https://graph.facebook.com/v10.0/oauth/access_token")
+            .userInfoUri("https://graph.facebook.com/me")
+            .userNameAttributeName("id,email")
+            .clientName("Facebook")
+            .build();
+	    }
 	
 	@Bean
 	public RestTemplate template() {
