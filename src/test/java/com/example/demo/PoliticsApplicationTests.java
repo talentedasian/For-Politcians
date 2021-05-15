bc5c00455086593d55f81d917673d56710c0a494
talentedasian <lucenaalmirjames123@gmail.com>
2021-05-15 15:02:22 +0800

    add info description swagger ui test

--- src/test/java/com/example/demo/PoliticsApplicationTests.java
+++ src/test/java/com/example/demo/PoliticsApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PoliticsApplicationTests {
 
+	@Autowired
+	public MockMvc mvc;
+	
 	@Test
-	void contextLoads() {
+	public void shouldReturn200IsOk() throws Exception {
+		mvc.perform(get("/swagger-ui/index.html"))
+			.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldReturnExpectedInfo() throws Exception {
+		String infoMessage = "use this jwt ";
+		mvc.perform(get("/v3/api-docs"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("info.description", 
+					containsStringIgnoringCase(infoMessage)));
 	}
 
 }
