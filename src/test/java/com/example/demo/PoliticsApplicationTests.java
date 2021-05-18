088c6678ab0227b00e96e76a6b9b99805047a3d8
talentedasian <lucenaalmirjames123@gmail.com>
2021-05-18 09:59:33 +0800

    remove openapi tests

--- src/test/java/com/example/demo/PoliticsApplicationTests.java
+++ src/test/java/com/example/demo/PoliticsApplicationTests.java
@@ -1,30 +1,11 @@
 package com.example.demo;
 
-import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class PoliticsApplicationTests {
 
-	@Autowired
-	public MockMvc mvc;
-
-	@Test
-	public void shouldReturnExpectedInfo() throws Exception {
-		String infoMessage = "use this jwt ";
-		mvc.perform(get("/v3/api-docs"))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("info.description", 
-					containsStringIgnoringCase(infoMessage)));
-	}
 
 }
