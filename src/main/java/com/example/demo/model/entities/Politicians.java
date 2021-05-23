b467acd78556959394012feaf85ce1b47b7e93e0
talentedasian <lucenaalmirjames123@gmail.com>
2021-05-23 11:27:18 +0800

    refactor politicians

--- src/main/java/com/example/demo/model/entities/Politicians.java
+++ src/main/java/com/example/demo/model/entities/Politicians.java
@@ -24,15 +24,6 @@ import com.example.demo.repository.RatingRepository;
 public class Politicians implements PoliticianMethods{
 
 	@Autowired transient RatingRepository repo;
-	private transient  AverageCalculator averageCalculator;
-	
-	public AverageCalculator getAverageCalculator() {
-		return averageCalculator;
-	}
-	
-	public void setAverageCalculator(AverageCalculator averageCalculator) {
-		this.averageCalculator = averageCalculator;
-	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -56,32 +47,13 @@ public class Politicians implements PoliticianMethods{
 	@Column(nullable = false, precision = 3, scale = 2)
 	private Double totalRating;
 
+
+	public RatingRepository getRepo() {
+		return repo;
+	}
+
 	public void setRepo(RatingRepository repo) {
 		this.repo = repo;
-	}
-
-	public String getFullName() {
-		return fullName;
-	}
-
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
-
-	public List<PoliticiansRating> getPoliticiansRating() {
-		return politiciansRating;
-	}
-
-	public void setPoliticiansRating(List<PoliticiansRating> politiciansRating) {
-		this.politiciansRating = politiciansRating;
-	}
-
-	public Double getTotalRating() {
-		return totalRating;
-	}
-
-	public void setTotalRating(Double totalRating) {
-		this.totalRating = totalRating;
 	}
 
 	public Integer getId() {
@@ -116,21 +88,28 @@ public class Politicians implements PoliticianMethods{
 		this.lastName = lastName;
 	}
 
-	public Politicians() {
-		super();
-		// TODO Auto-generated constructor stub
+	public String getFullName() {
+		return fullName;
 	}
 
-	public Politicians(Integer id, Double rating, String firstName, String lastName,
-			List<PoliticiansRating> politiciansRating, Double totalRating) {
-		super();
-		this.id = id;
-		this.rating = rating;
-		this.firstName = firstName;
-		this.lastName = lastName;
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public List<PoliticiansRating> getPoliticiansRating() {
+		return politiciansRating;
+	}
+
+	public void setPoliticiansRating(List<PoliticiansRating> politiciansRating) {
 		this.politiciansRating = politiciansRating;
+	}
+
+	public Double getTotalRating() {
+		return totalRating;
+	}
+
+	public void setTotalRating(Double totalRating) {
 		this.totalRating = totalRating;
-		
 	}
 	
 	public Politicians(Integer id, Double rating, String firstName, String lastName,
@@ -144,72 +123,28 @@ public class Politicians implements PoliticianMethods{
 		this.totalRating = totalRating;
 		this.repo = repo;
 	}
+	
+	public Politicians(Integer id, Double rating, String firstName, String lastName,
+			List<PoliticiansRating> politiciansRating, Double totalRating) {
+		super();
+		this.id = id;
+		this.rating = rating;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.politiciansRating = politiciansRating;
+		this.totalRating = totalRating;
+	}
 
+	public Politicians() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 	@Override
 	public String toString() {
 		return "Politicians [id=" + id + ", rating=" + rating + ", firstName=" + firstName + ", lastName=" + lastName
-				+ ", politiciansRating=" + politiciansRating + ", totalRating=" + totalRating + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-		result = prime * result + ((politiciansRating == null) ? 0 : politiciansRating.hashCode());
-		result = prime * result + ((rating == null) ? 0 : rating.hashCode());
-		result = prime * result + ((totalRating == null) ? 0 : totalRating.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Politicians other = (Politicians) obj;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (fullName == null) {
-			if (other.fullName != null)
-				return false;
-		} else if (!fullName.equals(other.fullName))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		if (politiciansRating == null) {
-			if (other.politiciansRating != null)
-				return false;
-		} else if (!politiciansRating.equals(other.politiciansRating))
-			return false;
-		if (rating == null) {
-			if (other.rating != null)
-				return false;
-		} else if (!rating.equals(other.rating))
-			return false;
-		if (totalRating == null) {
-			if (other.totalRating != null)
-				return false;
-		} else if (!totalRating.equals(other.totalRating))
-			return false;
-		return true;
+				+ ", fullName=" + fullName + ", totalRating=" + totalRating
+				+ "]";
 	}
 
 	@Override
