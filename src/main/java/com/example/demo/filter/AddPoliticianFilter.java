d2526e369ee64bb1cafc1f80f301a5bb8780c98c
talentedasian <lucenaalmirjames123@gmail.com>
2021-06-07 10:35:16 +0800

    Add Spring Doc Test
    Test if description matches swagger open api description

--- src/main/java/com/example/demo/filter/AddPoliticianFilter.java
+++ src/main/java/com/example/demo/filter/AddPoliticianFilter.java
@@ -1,7 +1,6 @@
 package com.example.demo.filter;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
--- src/test/java/com/example/demo/PoliticsApplicationTests.java
+++ src/test/java/com/example/demo/PoliticsApplicationTests.java
@@ -1,11 +1,27 @@
 package com.example.demo;
 
+import static java.net.URI.create;
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class PoliticsApplicationTests {
 
-
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	public void testSpringDocDescriptionApi() throws Exception {
+		mvc.perform(get(create("/v3/api-docs")))
+			.andExpect(jsonPath("info.description", 
+					containsStringIgnoringCase("Login with `oauth2/authorization/facebook` first to get a")));
+	}
 }
