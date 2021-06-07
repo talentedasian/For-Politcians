d1eef2741b2b4ac67cbcfe166a3beb33a18719d1
talentedasian <lucenaalmirjames123@gmail.com>
2021-06-07 11:12:36 +0800

    Fix test on Spring Doc Api
    Tests work on local machine but not on Github Actions. Changed the way how
    the Mock Mvc is loaded into the integration test

--- src/test/java/com/example/demo/PoliticsApplicationTests.java
+++ src/test/java/com/example/demo/PoliticsApplicationTests.java
@@ -5,18 +5,29 @@ import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class PoliticsApplicationTests {
 
-	@Autowired
 	private MockMvc mvc;
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
+	@BeforeEach
+	public void setup() {
+		mvc = MockMvcBuilders.webAppContextSetup(wac)
+				.build();
+	}
 	
 	@Test
 	public void testSpringDocDescriptionApi() throws Exception {
