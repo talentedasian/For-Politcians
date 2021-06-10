1fbafb449b1243ff72006d37ea43403be1b9354c
talentedasian <lucenaalmirjames123@gmail.com>
2021-06-10 17:05:35 +0800

    refactor test from null value to string value

--- src/test/java/com/example/demo/unit/controller/PoliticianControllerTest.java
+++ src/test/java/com/example/demo/unit/controller/PoliticianControllerTest.java
@@ -33,8 +33,8 @@ public class PoliticianControllerTest extends AbstractPoliticianControllerTest {
 		
 		assertThat(politician.getRating().getAverageRating(),
 				equalTo(politicianResponse.getBody().getRating()));
-		assertThat(politician.getId(),
-				nullValue());
+		assertThat(politicianResponse.getBody().getId(),
+				equalTo("null"));
 	}		
 	
 	@Test
