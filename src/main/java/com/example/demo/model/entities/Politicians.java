ab4c2ee786b606285dd866478f251cdbc62ac22d
talentedasian <lucenaalmirjames123@gmail.com>
2021-06-13 17:50:05 +0800

    Add an index for Politicians Entity
    Added the politician number as an index from the politicians
    table/entity. Subject to change or removal because of limited storage in
    heroku postgres.

--- src/main/java/com/example/demo/model/entities/Politicians.java
+++ src/main/java/com/example/demo/model/entities/Politicians.java
@@ -7,7 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -19,6 +21,7 @@ import com.example.demo.model.averageCalculator.LowSatisfactionAverageCalculator
 import com.example.demo.repository.RatingRepository;
 
 @Entity
+@Table(indexes = @Index(columnList = "politicianNumber") )
 public class Politicians implements PoliticianMethods{
 
 	@Autowired private transient RatingRepository repo;
