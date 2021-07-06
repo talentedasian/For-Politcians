1b7e97f66230753f1de7fd27dd73d31fb439dda3
talentedasian <lucenaalmirjames123@gmail.com>
2021-07-06 13:03:31 +0800

    Change @SpringBootTest of oauth2ControllerTest to a slice test using @WebMvcTest

--- src/test/java/com/example/demo/integration/controllers/Oauth2ControllerTest.java
+++ src/test/java/com/example/demo/integration/controllers/Oauth2ControllerTest.java
@@ -1,26 +1,36 @@
 package com.example.demo.integration.controllers;
 
 import static java.net.URI.create;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import javax.servlet.http.Cookie;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import com.example.demo.controller.Oauth2;
 import com.example.demo.jwt.JwtProvider;
 
-@SpringBootTest
-@AutoConfigureMockMvc(print = MockMvcPrint.DEFAULT, printOnlyOnFailure = false)
+@WebMvcTest({Oauth2.class})
 public class Oauth2ControllerTest {
 
-	@Autowired public MockMvc mvc;
+	public MockMvc mvc;
+	
+	@BeforeEach
+	public void setup(WebApplicationContext wac) {
+		mvc = MockMvcBuilders.webAppContextSetup(wac)
+				.alwaysDo(MockMvcResultHandlers.print())
+				.build();
+	}
 	
 	@Test
 	public void shouldReturnHalForms() throws Exception {
@@ -31,7 +41,9 @@ public class Oauth2ControllerTest {
 		mvc.perform(get(create("/login/oauth2/code/facebook"))
 				.accept(MediaTypes.HAL_FORMS_JSON)
 				.cookie(cookies))
-			.andExpect(status().isOk());
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("jwt",
+					equalTo(jwt)));
 	}
 	
 }
