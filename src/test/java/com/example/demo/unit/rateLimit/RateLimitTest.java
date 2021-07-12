dceeefbfbf08348d919d72d2424117a8e9d87a31
talentedasian <lucenaalmirjames123@gmail.com>
2021-07-12 13:55:25 +0800

    Test daysLeft ratelimit

--- src/test/java/com/example/demo/unit/rateLimit/RateLimitTest.java
+++ src/test/java/com/example/demo/unit/rateLimit/RateLimitTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.unit.rateLimit;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
@@ -45,6 +46,13 @@ public class RateLimitTest {
 		rate.setDateCreated(LocalDate.now().minusDays(5L));
 		
 		assertFalse(rate.isNotRateLimited());
+	}
+	
+	@Test
+	public void shouldBeNullDaysLeft() {
+		var rate = RateLimit.withNotExpiredRateLimit("TGFLM-00000000000123", "123polNumber");
+		
+		assertNull(rate.daysLeftOfBeingRateLimited());
 	}
 	
 }
