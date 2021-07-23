2b0390f14a2ad474a7f2427de58afacbb2febce0
talentedasian <lucenaalmirjames123@gmail.com>
2021-07-24 07:50:05 +0800

    Add logback configuration
    Log name and politician number in file "politicians.log" whenever a
    politician is saved.

---
+++ src/main/java/com/example/demo/logger/PoliticiansLogger.java
@@ -0,0 +1,26 @@
+package com.example.demo.logger;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.jboss.logging.MDC;
+import org.slf4j.LoggerFactory;
+
+import com.example.demo.model.entities.Politicians;
+
+@Aspect
+public class PoliticiansLogger {
+
+	@AfterReturning(
+			pointcut = "execution(* com.example.demo.service.PoliticiansService.savePolitician(..))",
+			returning = "politician"
+			)
+	public void logSuccessfulSavedPolitician(Politicians politician) {
+		MDC.put("fullname", politician.getFullName());
+		MDC.put("polNumber", politician.getPoliticianNumber());
+		
+		LoggerFactory.getLogger(PoliticiansLogger.class).info("saved with");
+		
+		MDC.clear();
+	}
+
+}
