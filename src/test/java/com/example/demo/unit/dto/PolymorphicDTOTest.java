7ecda21196e0d31570da0b420baacc7d171522cf
talentedasian <lucenaalmirjames123@gmail.com>
2021-08-04 11:03:54 +0800

    Test mapping of polymorphic politicians to dtos

---
+++ src/test/java/com/example/demo/unit/dto/PolymorphicDTOTest.java
@@ -0,0 +1,52 @@
+package com.example.demo.unit.dto;
+
+import com.example.demo.model.entities.politicians.PoliticianTypes;
+import com.example.demo.model.entities.politicians.Politicians;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PolymorphicDTOTest extends BaseClassForPoliticianDTOTests {
+
+    @Test
+    public void testPolymorphicDTOMapper() {
+        var presidentialPolitician = new PoliticianTypes.PresidentialPolitician
+                .PresidentialBuilder(politicianBuilder
+                    .setFirstName("Ninoy")
+                    .setLastName("Aquino")
+                    .setFullName())
+                .build();
+
+        var senatorialPolitician = new PoliticianTypes.SenatorialPolitician
+                .SenatorialBuilder(politicianBuilder
+                    .setFirstName("Nancy")
+                    .setLastName("Binay")
+                    .setFullName())
+                .setTotalMonthsOfService(monthsOfService)
+                .build();
+
+        List<Politicians> listOfPolymorphicPoliticians = List.of(presidentialPolitician,senatorialPolitician);
+
+        assertThat(streamOfPoliticians())
+                .containsAll(listOfPolymorphicPoliticians);
+    }
+
+    private List<Politicians> streamOfPoliticians() {
+        return List.of(
+                senatorialBuilder
+                        .setBuilder(politicianBuilder
+                                .setFirstName("Nancy")
+                                .setLastName("Binay")
+                                .setFullName())
+                        .buildWithDifferentBuilder(),
+                presidentialBuilder
+                        .setBuilder(politicianBuilder
+                                .setFirstName("Ninoy")
+                                .setLastName("Aquino")
+                                .setFullName())
+                        .buildWithDifferentBuilder());
+    }
+
+}
