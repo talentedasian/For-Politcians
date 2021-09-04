bc6da73183633366e4fb337ce15bb6a471063337
talentedasian <lucenaalmirjames123@gmail.com>
2021-09-04 22:31:15 +0800

    Add object isEqualTo methods

--- src/test/java/com/example/demo/baseClasses/MockMvcAssertions.java
+++ src/test/java/com/example/demo/baseClasses/MockMvcAssertions.java
@@ -1,23 +1,27 @@
 package com.example.demo.baseClasses;
 
 import org.assertj.core.api.AbstractAssert;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.io.UnsupportedEncodingException;
 
 public class MockMvcAssertions extends AbstractAssert<MockMvcAssertions, MockMvcResult> {
 
     private static MockMvcResult actual;
 
-    protected MockMvcAssertions(MockMvcResult mockMvcResult) {
-        super(mockMvcResult, MockMvcAssertions.class);
-        this.actual = mockMvcResult;
+    protected MockMvcAssertions(MvcResult mockMvcResult) throws UnsupportedEncodingException {
+        super(MockMvcResult.of(mockMvcResult.getResponse().getContentAsString()), MockMvcAssertions.class);
+        this.actual = MockMvcResult.of(mockMvcResult.getResponse().getContentAsString());
     }
 
-    public static MockMvcAssertions assertThat(MockMvcResult actual) {
+    public static MockMvcAssertions assertThat(MvcResult actual) throws UnsupportedEncodingException {
         return new MockMvcAssertions(actual);
     }
 
     public MockMvcAssert hasPath(String path) {
         return MockMvcAssert.of(path, actual);
     }
+
 
 }
 
