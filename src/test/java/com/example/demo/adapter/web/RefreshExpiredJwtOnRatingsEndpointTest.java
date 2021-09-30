36196b98e9437cfb5ee3595a427045eb8d2397fc
talentedasian <lucenaalmirjames123@gmail.com>
2021-09-30 08:59:03 +0800

    Rename test method

--- src/test/java/com/example/demo/adapter/web/RefreshExpiredJwtOnRatingsEndpointTest.java
+++ src/test/java/com/example/demo/adapter/web/RefreshExpiredJwtOnRatingsEndpointTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class RefreshExpiredJwtTest extends BaseSpringHateoasTest {
+public class RefreshExpiredJwtOnRatingsEndpointTest extends BaseSpringHateoasTest {
 
     AddRatingDTORequest objectRequestContent = new AddRatingDTORequest(BigDecimal.ONE, "1", "DDS");
     String requestContent;
