5c3b78f36a8d8575937a41d0717404ddc56ee17f
talentedasian <lucenaalmirjames123@gmail.com>
2021-10-19 11:08:18 +0800

    Remove test

--- src/test/java/testAnnotations/TestBean.java
+++
@@ -1,9 +0,0 @@
-package testAnnotations;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-
-@Bean
-@Primary
-public @interface TestBean {
-}
