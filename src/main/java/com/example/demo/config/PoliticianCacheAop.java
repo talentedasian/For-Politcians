d314c235c1baf38c2e53e3bcac8818507b8235f9
talentedasian <lucenaalmirjames123@gmail.com>
2021-12-04 20:53:28 +0800

    Update cache values whenever a user rates a politician. Still implemented using AOP.

--- src/main/java/com/example/demo/config/PoliticianCacheAop.java
+++ src/main/java/com/example/demo/config/PoliticianCacheAop.java
@@ -2,10 +2,13 @@ package com.example.demo.config;
 
 import com.example.demo.domain.entities.Politicians;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.ehcache.Cache;
 import org.ehcache.core.spi.service.StatisticsService;
+
+import java.util.Optional;
 
 @Aspect
 public class PoliticianCacheAop {
@@ -23,12 +26,21 @@ public class PoliticianCacheAop {
         String polNumber = String.valueOf(jointP.getArgs()[0]);
         System.out.println(stats.getCacheStatistics("research").getCacheHitPercentage() + " cache hit percentage");
         if (cache.containsKey(polNumber))
-            return cache.get(polNumber);
+            return Optional.of(cache.get(polNumber));
 
         Object response = jointP.proceed();
-        cache.put(polNumber, (Politicians) response);
+        System.out.println(response);
+        Politicians methodResponse = (((Optional<? extends Politicians>) response)).get();
+        cache.put(polNumber, methodResponse);
 
         return response;
+    }
+
+    @AfterReturning(pointcut = "execution (* com.example.demo.adapter.out.repository.PoliticianJpaAdapterRepository.update(..))",
+                    returning = "politician")
+    public void updateCacheValue(Politicians politician) throws Throwable {
+        System.out.println(cache.containsKey(politician.retrievePoliticianNumber()) + " contains ba to");
+        cache.put(politician.retrievePoliticianNumber(), politician);
     }
 
 }
